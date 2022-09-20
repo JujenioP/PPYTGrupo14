fcd37906d6d2cd83cc1c6851a9ed1e126ae555f2
JujenioP <110836213+JujenioP@users.noreply.github.com>
2022-09-20 16:51:28 -0300

    Creado la Funcion Team

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from './Home'
-
+import Team from './Team'
 function App() {
   return (
     <div>
       <Home/>
+      <Team/>
     </div>
   );
 }
---
+++ src/Team.js
@@ -0,0 +1,31 @@
+function Team() {
+    return (
+        <div className="container">
+            <h1>Equipo de Trabajo</h1>
+            <div>
+                <div>
+                    <img />
+                    <h3>Maximiliano Abel Mamani</h3>
+                </div>
+                <div>
+                    <img />
+                    <h3>Matias Gabriel Ortega</h3>
+                </div>
+                <div>
+                    <img />
+                    <h3>Maximiliano Leonel Bustamante</h3>
+                </div>
+                <div>
+                    <img />
+                    <h3>Agustina Morales</h3>
+                </div>
+                <div>
+                    <img />
+                    <h3>Matias Alan Cruz Aruzamen</h3>
+                </div>
+            </div>
+            <button>Volver</button>
+        </div>
+    )
+}
+export default Team;
