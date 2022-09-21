38ff81aba2c75a10a5d1c513ad41bcbb29a7fa6a
MatiasGrrrOrtega <mat-1998-ort@hotmail.com>
2022-09-21 17:07:21 -0300

    aplicando cambios

--- src/App.js
+++ src/App.js
@@ -1,20 +1,3 @@
-
-import Home from './Home'
-import Team from './Team';
-import {Route, Routes} from 'react-router-dom'
-function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Team' element={<Team/>}/>
-      </Routes>
-    </div>
-import Home from "./component/Home";
-import Game from "./component/Game";
-import Team from "./component/Team";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 export default function App() {
   return (
     <BrowserRouter>
@@ -24,6 +7,5 @@ export default function App() {
         <Route path="/Team" element={ <Team/>} />
       </Routes>
     </BrowserRouter>
->>>>>>> 6da5bf379343a6c8eb5aea4164860c42931ececf
   );
 }
