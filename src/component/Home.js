063bd23c1e551ab767efdd748b70332d1c0635ff
Agmor03 <112333132+Agmor03@users.noreply.github.com>
2022-09-22 16:58:32 -0300

    Hecho CSSs y una carpeta de imagenes

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-
+import '../styles/Home.css'
 
 export default function Home() {
   return (
     <div className="Inicio">
         <h1>PIEDRA, PAPEL O TIJERA</h1>
-        <img/>
+        <img src="https://cdn-icons-png.flaticon.com/512/6688/6688560.png" alt="manos"/>
         <Link to={'/Game'} className="btn-principal "> Jugar </Link>
         <Link to={'/Team'} className="btn-principal "> Desarrolladores </Link>
     </div>
