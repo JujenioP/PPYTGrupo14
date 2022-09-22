564e37a61bd5f3fcc32ce923918584aabd848249
JujenioP <110836213+MaxAbelMamani@users.noreply.github.com>
2022-09-22 17:44:27 -0300

    Modificado el Home css y Team css

--- src/component/Team.js
+++ src/component/Team.js
@@ -1,28 +1,28 @@
 import { Link } from "react-router-dom";
-
+import '../styles/Team.css'
 export default function Team() {
     return (
         <div className="container">
             <h1>Equipo de Trabajo</h1>
             <div>
-                <div>
-                    <img />
+                <div className="NyF">
+                    <img src="../images/Matias_Ortega.jpg" alt="..."/>
                     <h3>Maximiliano Abel Mamani</h3>
                 </div>
-                <div>
-                    <img />
+                <div className="NyF">
+                    <img src="../images/Matias_Ortega.jpg" alt="..."/>
                     <h3>Matias Gabriel Ortega</h3>
                 </div>
-                <div>
-                    <img />
-                    <h3>Maximiliano Leonel Bustamante</h3>
+                <div className="NyF">
+                    <img src="../images/Matias_Ortega.jpg" alt="..."/>
+                    <h3>Maximiliano L. Bustamante</h3>
                 </div>
-                <div>
-                    <img />
-                    <h3>Agustina Morales</h3>
+                <div className="NyF">
+                    <img src="../images/Matias_Ortega.jpg" alt="..."/>
+                    <h3>Agustina M. A. Morales</h3>
                 </div>
-                <div>
-                    <img />
+                <div className="NyF">
+                    <img src="../images/Matias_Ortega.jpg" alt="..."/>
                     <h3>Matias Alan Cruz Aruzamen</h3>
                 </div>
             </div>
