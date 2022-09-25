464e471db1b6c30fd27f56c4d97b9ef57e0573c3
MatiasGrrrOrtega <mat-1998-ort@hotmail.com>
2022-09-24 21:51:54 -0300

    Arreglando un poco el CSS de las paginas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./component/Home";
 import Game from "./component/Game";
 import Team from "./component/Team";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
--- src/component/Home.js
+++ src/component/Home.js
@@ -3,14 +3,13 @@ import '../styles/Home.css'
 import '../styles/Button.css'
 export default function Home() {
   return (
-    <div className="Inicio">
-      <br></br>
-      <br></br>
+    <div className="container-inicio">
       <h1>PIEDRA, PAPEL O TIJERA</h1>
       <img src="https://cdn-icons-png.flaticon.com/512/6688/6688560.png" alt="manos"/>
-      <Link to={'/Game'} className="btn-principal"> Jugar </Link>
-      <br></br>
-      <Link to={'/Team'} className="btn-principal"> Desarrolladores </Link>
+      <div className="botones-home">
+        <Link to={'/Game'} className="btn"> Jugar </Link>
+        <Link to={'/Team'} className="btn"> Desarrolladores </Link>
+      </div>
     </div>
   )
 }
